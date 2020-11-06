460c884296cae463b3dc6a50f37248a98a4ead29
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-11-06 18:34:14 +0100

    idataset

--- translate_lyrics.py
+++ translate_lyrics.py
@@ -1,18 +1,40 @@
 import pandas as pd
 from googletrans import  Translator
+from tqdm import tqdm
+import time
 # from langdetect import detect
 
-def translate(row):
-    if not pd.isnull(row["lyrics"]):
-        # lang = detect(row["lyrics"])
-        if "nigga" in row["lyrics"].lower() or "fuck" in row["lyrics"].lower() or "neuken" in row["lyrics"].lower() or "money" in row["lyrics"].lower():
-            row["lyrics"] = ""
-        else:
-            row["lyrics"] = translator.translate(row["lyrics"], dest='en').text
-    return row
+# def translate(row):
+#     if not pd.isnull(row["lyrics"]):
+#         # lang = detect(row["lyrics"])
+#         if "nigga" in row["lyrics"].lower() or "fuck" in row["lyrics"].lower() or "neuken" in row["lyrics"].lower() or "money" in row["lyrics"].lower():
+#             row["lyrics"] = ""
+#         else:
+#             row["lyrics"] = translator.translate(row["lyrics"], dest='en').text
+#     return row
 
 if __name__ == "__main__":
-    df = pd.read_csv("./lyrics.csv")
+    df = pd.read_csv("./lyrics_dataset.csv")
     translator = Translator()
-    df = df.apply(translate, axis=1)
+    # df = df.apply(translate, axis=1)
+
+    with open("lyrics_translated.txt", "w") as f:
+        for _, row in tqdm(df.iterrows(), total=len(df)):
+
+            if type(row["lyrics"]) == str:
+                if "nigga" in row["lyrics"].lower() or "fuck" in row["lyrics"].lower() or "neuken" in row["lyrics"].lower() or "money" in row["lyrics"].lower():
+                    continue
+
+                lyrics = row["lyrics"]
+
+                try:
+                    lyrics_nl = translator.translate(lyrics, dest='en').text
+                    f.write(lyrics_nl)
+                    f.write(" <|endoftext|> ")
+                    f.write("\n")
+                except Exception as e:
+                    print("Failed {}".format(row["song"]))
+                    print(e)
+                    time.sleep(20)
+
     df.to_csv("lyrics_translated.csv", index=False)
